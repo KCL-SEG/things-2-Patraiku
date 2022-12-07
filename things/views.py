09adacd1c319b9f949f8db7e51ff553edd3ca1df
Patryk Kugla <patryk.kugla@kcl.ac.uk>
2022-12-07 03:08:29 +0000

    Complete exercises

--- things/views.py
+++ things/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .forms import ThingForm
 
 def home(request):
-    return render(request, 'home.html', {'form': form})
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'home.html', {'form': ThingForm()})
+        else:
+            return render(request, 'home.html', {'form': form})
+    return render(request, 'home.html', {'form': ThingForm()})
